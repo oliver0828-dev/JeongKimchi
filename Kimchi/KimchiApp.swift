bd7d3d0e087d4a72656bcb1cdd4a504f178a74ac
Oliver Park <oliverco0828@gmail.com>
2024-12-09 20:46:53 +0900

    Initial Commit

---
+++ Kimchi/KimchiApp.swift
@@ -0,0 +1,17 @@
+//
+//  KimchiApp.swift
+//  Kimchi
+//
+//  Created by Oliver Park on 12/9/24.
+//
+
+import SwiftUI
+
+@main
+struct KimchiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ KimchiTests/KimchiTests.swift
@@ -0,0 +1,17 @@
+//
+//  KimchiTests.swift
+//  KimchiTests
+//
+//  Created by Oliver Park on 12/9/24.
+//
+
+import Testing
+@testable import Kimchi
+
+struct KimchiTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
